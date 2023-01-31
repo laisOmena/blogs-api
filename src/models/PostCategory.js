a929df05515dcaea72cba576bf5bd2ac50b4358b
Lais Omena <laisf.omena@gmail.com>
2023-01-31 17:01:54 +0000

    Requisito 11

---
+++ src/models/PostCategory.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', 
+  {
+    postId: { type: DataTypes.INTEGER, foreignKey: true },
+    categoryId: { type: DataTypes.INTEGER, foreignKey: true },
+    createdAt: DataTypes.Date,
+    updatedAt: DataTypes.Date
+  },
+  {
+    tableName: 'posts_categories',
+    underscored: true,
+    
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blog_posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId'
+      
+    });
+
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId'
+      
+    });
+  };
+
+
+  return PostCategory;
+};
